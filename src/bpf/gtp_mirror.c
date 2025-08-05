77b7320968cfd2ca444cb2233261e6d06b1934fb
Olivier Gournet <gournet.olivier@gmail.com>
2025-08-05 08:00:08 +0200

    bpf: move bpf programs to lib
    Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

--- src/bpf/gtp_mirror.c
+++ src/bpf/gtp_mirror.c
@@ -16,75 +16,15 @@
  *              either version 3.0 of the License, or (at your option) any later
  *              version.
  *
- * Copyright (C) 2023-2024 Alexandre Cassen, <acassen@gmail.com>
+ * Copyright (C) 2023-2025 Alexandre Cassen, <acassen@gmail.com>
  */
 
-#define KBUILD_MODNAME "gtp_mirror"
-#include <stddef.h>
-#include <stdbool.h>
-#include <string.h>
-#include <linux/bpf.h>
-#include <linux/pkt_cls.h>
-#include <linux/in.h>
-#include <linux/if_ether.h>
-#include <linux/if_packet.h>
-#include <linux/if_vlan.h>
-#include <linux/ip.h>
-#include <linux/udp.h>
-#include <sys/socket.h>
-#include <bpf_endian.h>
-#include <bpf_helpers.h>
-#include "gtp.h"
+#include "lib/gtp_mirror.h"
 
-
-/*
- *	MAPs
- */
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, MAX_MIRROR_ENTRIES);
-	__type(key, __be32);
-	__type(value, struct gtp_mirror_rule);
-} mirror_rules SEC(".maps");
-
-
-SEC("tc")
+SEC("tcx/ingress")
 int tc_gtp_mirror(struct __sk_buff *skb)
 {
-	void *data = (void *)(long)skb->data;
-	void *data_end = (void *)(long)skb->data_end;
-	int offset = sizeof(struct ethhdr);
-	struct iphdr *iph;
-	struct udphdr *udph;
-	struct gtp_mirror_rule *rule;
-
-	if (unlikely(skb->protocol != __constant_htons(ETH_P_IP)))
-		return TC_ACT_OK;
-
-	iph = data + offset;
-	if (iph + 1 > data_end)
-		return TC_ACT_OK;
-
-	/* First match destination address */
-	rule = bpf_map_lookup_elem(&mirror_rules, &iph->daddr);
-	rule = (rule) ? rule : bpf_map_lookup_elem(&mirror_rules, &iph->saddr);
-	if (!rule)
-		return TC_ACT_OK;
-
-	if (iph->protocol != rule->protocol)
-		return TC_ACT_OK;
-
-	offset += sizeof(struct iphdr);
-	udph = data + offset;
-	if (udph + 1 > data_end)
-		return TC_ACT_OK;
-
-	if (!(udph->dest == rule->port || udph->source == rule->port))
-        	return TC_ACT_OK;
-
-	bpf_clone_redirect(skb, rule->ifindex, 0);
-
-	return TC_ACT_OK;
+	return gtp_mirror(skb);
 }
 
 const char _mode[] = "gtp_mirror";
