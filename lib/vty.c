57972bb79dc0f3d9c4fb0208070883a692b214ed
Olivier Gournet <gournet.olivier@gmail.com>
2025-08-13 03:22:28 +0200

    lib/vty: exit from config_from_file on CMD_WARNING
    The main usage of CMD_WARNING is to indicate an error in a vty callback:
    bad argument, operation cannot be done, ...
    
    When reading initial configuration file (vty_read_config()), returning
    CMD_WARNING will stop parsing, thus avoid warning/errors on next line that
    could be confusing.
    
    Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

--- lib/vty.c
+++ lib/vty.c
@@ -1665,8 +1665,9 @@ vty_read_file(FILE *confp)
 			log_message(LOG_ERR, "There is no such command.\n");
 			break;
 		}
-		log_message(LOG_ERR, "Error occured during reading below line.\n%s\n"
-				   , vty->buf);
+		if (ret != CMD_WARNING)
+			log_message(LOG_ERR, "Error occured during reading below line.\n%s\n"
+				           , vty->buf);
 		vty_close(vty);
 		return -1;
 	}
@@ -1800,7 +1801,9 @@ vty_read_config(char *config_file, char *config_default_dir)
 		}
 	}
 
-	vty_read_file(confp);
+	if (vty_read_file(confp) < 0)
+		log_message(LOG_ERR, "%s: error loading configuration file"
+			           , fullpath);
 	fclose(confp);
 	host_config_set(fullpath);
 	FREE_PTR(tmp);
